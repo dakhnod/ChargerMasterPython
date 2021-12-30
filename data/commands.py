ae3061f115bcb1ffabdacf969d2c418501ae194b
Daniel Dakhno <dakhnod@gmail.com>
2021-12-30 17:24:34 +0100

    added set_use_balance_wire api

--- data/commands.py
+++ data/commands.py
@@ -1,17 +1,23 @@
+CHANNEL_NUM_ALL = -1
+
 def get_system_info(channel_num: int):
-    return create_packet(90, [1 << channel_num])
+    return create_packet(90, channel_num)
 
 
 def get_channel_status(channel_num: int):
-    return create_packet(85, [1 << channel_num])
+    return create_packet(85, channel_num)
 
 
 def get_voltage(channel_num: int):
-    return create_packet(88, [1 << channel_num])
+    return create_packet(88, channel_num)
+
+
+def set_use_balance_wire(channel_num: int, use_balance_wire: bool):
+    return create_packet(0x11, channel_num, [0x06, 0x01 if use_balance_wire else 0x00, 0x00, 0x00])
 
 
 def start_charge(channel_num: int, battery_type: int, charge_type: int, cell_count: 4, charge_current_ma: int, discharge_current_ma: int, voltage_limit_lower: int, voltage_limit_upper: int):
-    payload = [1 << channel_num]
+    payload = []
     payload.append(battery_type)
     payload.append(cell_count)
     payload.append(charge_type)
@@ -24,7 +30,7 @@ def start_charge(channel_num: int, battery_type: int, charge_type: int, cell_cou
     payload.append(0x0A)
     payload.append(0x04)
     payload += [0x00, 0x00, 0x00, 0x00]
-    return create_packet(0x05, payload)
+    return create_packet(0x05, channel_num, payload)
 
 
 def start_charge_lipo(channel_num: int, cell_count: int, charge_current_ma: int):
@@ -37,12 +43,15 @@ def start_storage_lipo(channel_num: int, cell_count: int, discharge_current_ma: 
 
 def stop_charge(channel_num: int = -1):
     if channel_num == -1:
-        return create_packet(0xFE, [0xFF])
-    return create_packet(0xFE, [1 << channel_num])
+        return create_packet(0xFE, CHANNEL_NUM_ALL)
+    return create_packet(0xFE, channel_num)
 
 
-def create_packet(type: int, data: list):
-    payload = [15, len(data) + 2, type] + data
+def create_packet(type: int, channel_num: int, data=None):
+    if data is None:
+        data = []
+    channel_num = 0xFF if channel_num == -1 else 1 << channel_num
+    payload = [15, len(data) + 3, type, channel_num] + data
     checksum = 0
     for i in range(2, len(payload)):
         checksum += payload[i]
@@ -55,5 +64,5 @@ def from_short(value: int):
 
 
 if __name__ == '__main__':
-    packet = get_status(3)
+    packet = set_use_balance_wire(3, False)
     pass
